11e0a5268a8e9a0c40b6b1c169aceeacad2150b0
JerryJieLi <137041856+JerryJieLi@users.noreply.github.com>
2023-09-09 17:22:27 +0800

    Add files via upload

---
+++ SG90/main.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<wiringPi.h>
+#include<softPwm.h>
+
+int pwmPin = 29;
+
+void init();
+int main()
+{
+    init();
+    softPwmWrite(pwmPin, 6);
+    delay(3000);
+    softPwmWrite(pwmPin, 15);
+    delay(3000);
+
+        //return 0;
+}
+
+
+void init()
+{
+        wiringPiSetup();
+        softPwmCreate(pwmPin, 0, 200);
+}
